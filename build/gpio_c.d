5983ce4be6013dd65aaf0057fe38c9bef10c47eb
knight <rishirajrajgor@gmail.com>
2025-07-23 17:14:20 +0530

    NovaPi

--- build/gpio_c.d
+++
@@ -1,2 +0,0 @@
-build/gpio_c.o: src/gpio.c include/peripherals/gpio.h \
- include/peripherals/base.h include/utils.h
--- build/kernel_main_c.d
+++
@@ -1,2 +0,0 @@
-build/kernel_main_c.o: src/kernel_main.c include/printf.h include/utils.h \
- include/mini_uart.h include/timer.h include/irq.h
