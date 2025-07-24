b6278bc3ee04e01b4188f7d1fc8d8d1f89df0a4a
moxybaba <mokshpanicker.7@gmail.com>
2025-07-24 17:54:46 +0530

    spi

---
+++ include/spi.h
@@ -0,0 +1,12 @@
+#ifndef SPI_H
+#define SPI_H
+
+extern void spi_init(void);
+extern void display_logo(void);
+extern void oled_command(void);
+extern void oled_data(void);
+extern void spi_write_byte(unsigned char data);
+extern void oled_clear_screen(void);
+extern void oled_init(void);
+
+#endif
