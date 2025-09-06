af18ecc87d8d83debe3d5ecf3c67ab7cac9d47d2
knight <rishirajrajgor@gmail.com>
2025-09-06 23:28:38 +0530

    NovaPi-OS

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-NovaPi
-# NovaPi
+# Learning operating system development using Linux kernel and Raspberry Pi
+
+The repository deals with creating a bare-metal operating system and hardware abstraction layer for Raspberry Pi 4B. The operating system will implement basic communication protocols, interrupt  controllers and ultimately HDMI support. Using these as the base this project will aim at creating a simple pong game using the HDMI Support of our kernel. This is still work in progress.
+
+### *TASKS*
+
+1. UART IMPLEMENTATION (BOTH PL011 AND MINI-UART)
+2. CHANGING EL STATUS FROM 2 TO 1
+3. INTERRUPT AND SYSTEM-TIMER
+4. SPI (Serial Peripheral Interface)
+5. I2C DRIVER
+6. MAILBOX AND VIDEO VIA HDMI(WORK IN PROGRESS)
+7. DMA (WORK IN PROGRESS)
+8. PING-PONG GAME (TO BE DONE)
+
+HOW TO START ON THIS PROJECT :
+1. Clone the repository in your computer
+2. Use command
+```bash
+     make clean && make
+     ```
+3. REQUIREMENTS : A MICRO SD CARD , and using RPI IMAGER flah the raspberry pi 64 os on it.
+4. AFTER MAKE A 'kernel8.img' will be generated copy this and the 'config.txt' files in sd card and you are ready to start on this project.
+
+HAPPY CODING !!!!!
--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,84 +1,27 @@
-#include "common.h"
-#include "mini_uart.h"
 #include "printf.h"
-#include "irq.h"
+#include "utils.h"
+#include "mini_uart.h"
 #include "timer.h"
-#include "i2c.h"
+#include "irq.h"
 #include "spi.h"
-#include "mailbox.h"
-#include "video.h"
+#include "pl011.h"
+#include "oled.h"
 
 
-void kernel_main(void){
-    
-    muart_init();//
-    init_printf(0,putc);  // Hook printf to UART output
+void kernel_main(void)
+{ 
+    muart_init();
+    init_printf(0,putc);  
     muart_send_string("Hello, world!\r\n");
     int el = get_el();
-    printf("Exception level: %d\r\n", el);  // Should now work
- 
+    printf("Exception level: %d\r\n", el);  
     
-    printf("MAILBOX:\n");
+    spi_init();
+    spi_write(0x96);
 
-    printf("CORE CLOCK: %d\n", mailbox_clock_rate(CT_CORE));
-    printf("EMMC CLOCK: %d\n", mailbox_clock_rate(CT_EMMC));
-    printf("UART CLOCK: %d\n", mailbox_clock_rate(CT_UART));
-    printf("ARM  CLOCK: %d\n", mailbox_clock_rate(CT_ARM));
 
-    printf("I2C POWER STATE:\n");
-
-    for (int i=0; i<3; i++) {
-        bool on = mailbox_power_check(i);
-
-        printf("POWER DOMAIN STATUS FOR %d = %d\n", i, on);
-    }
-
-    //timer_sleep(2000);
-
-    for (int i=0; i<3; i++) {
-        u32 on = 1;
-        mailbox_generic_command(RPI_FIRMWARE_SET_DOMAIN_STATE, i, &on);
-
-        printf("SET POWER DOMAIN STATUS FOR %d = %d\n", i, on);
-    }
-
-    //timer_sleep(1000);
-
-    for (int i=0; i<3; i++) {
-        bool on = mailbox_power_check(i);
-
-        printf("POWER DOMAIN STATUS FOR %d = %d\n", i, on);
-    }
-
-    u32 max_temp = 0;
-
-    mailbox_generic_command(RPI_FIRMWARE_GET_MAX_TEMPERATURE, 0, &max_temp);
-
-    //Do video...
-// Use more conservative resolutions first
-printf("Testing 640x480\n");
-video_set_resolution(640, 480, 32);
-
-printf("Testing 1024x768\n");
-video_set_resolution(1024, 768, 32);
-
-printf("Testing 1920x1080\n");
-video_set_resolution(1920, 1080, 32);
-
-    while(1) {
-        u32 cur_temp = 0;
-
-        mailbox_generic_command(RPI_FIRMWARE_GET_TEMPERATURE, 0, &cur_temp);
-
-        printf("Cur temp: %dC MAX: %dC\n", cur_temp / 1000, max_temp / 1000);
-
-        timer_sleep(1000);
-    }
+	while (1){
+		//uart_send(uart_recv());
+	}	
 }
-    
-
 	
-
-
-//qemu-system-aarch64 -M raspi3b -kernel kernel8.img -nographic -serial mon:stdio -serial null(pl011 command for QEMU)
-//qemu-system-aarch64     -M raspi3b   -kernel kernel8.img  -nographic   -serial null     -serial mon:stdio (mini uart command)
