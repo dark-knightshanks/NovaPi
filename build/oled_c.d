ce47e6559e8f5af62689d4bf00e1a001fc657d4a
knight <rishirajrajgor@gmail.com>
2025-09-06 23:28:57 +0530

    delete build files

--- build/oled_c.d
+++
@@ -1,2 +0,0 @@
-build/oled_c.o: src/oled.c include/printf.h include/peripherals/i2c.h \
- include/peripherals/base.h
--- build/video_c.d
+++
@@ -1,2 +0,0 @@
-build/video_c.o: src/video.c include/mailbox.h include/common.h \
- include/printf.h include/utils.h include/video.h
